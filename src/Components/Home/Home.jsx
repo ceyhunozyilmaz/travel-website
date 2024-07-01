59e342175b45d08dde45a9eb10465a46cc6f00cb
ceyhunozyilmaz <sanatsalsaat18@gmail.com>
2024-07-01 18:11:45 +0300

    homepage updated

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,16 +2,66 @@ import React from "react";
 import "./home.scss";
 import video from "../../assets/video2.mp4";
 
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
+
 const Home = () => {
   return (
     <section className="home">
-      <div className="overlay">
-        <video src={video} muted loop type="video/mp4"></video>
+      <video src={video} muted loop autoPlay type="video/mp4"></video>
+      <div className="overlay"></div>
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">Paketlerimiz</span>
+          <h1 className="homeTitle">Tatilinizi Planlayın</h1>
+        </div>
 
-        <div className="homeContent container">
-          <div className="textDiv">
-            <span className="smallText">Paketlerimiz</span>
-            <h1 className="homeTitle">Tatilinizi Planlayın</h1>
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Gideceğiniz yeri arayın:</label>
+            <div className="input flex">
+              <input type="text" placeholder="Buraya şehri  yazınız..." />
+              <GrLocation className="icon " color="grey" />
+            </div>
+          </div>
+
+          <div className="dateInput">
+            <label htmlFor="date">Tarihinizi seçin:</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+
+          <div className="priceInput ">
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
+          </div>
+
+          <div className="searchOptions flex">
+            <HiFilter className="icon" />
+            <span>DAHA FAZLA FİLTRELE</span>
+          </div>
+        </div>
+
+        <div className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook className="icon" />
+            <AiOutlineInstagram className="icon" />
+            <FaTripadvisor className="icon" />
+          </div>
+          <div className="leftIcons">
+            <BsListTask className="icon" />
+            <TbApps className="icon" />
           </div>
         </div>
       </div>
